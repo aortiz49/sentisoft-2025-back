e1c66e070ad37767c4ce49c9073ab50476efb81a
srojasre <s.rojasr2@uniandes.edu.co>
2025-03-14 10:53:19 -0500

    allowed origin change for real link, env *change*. gitignore for python

--- app/main.py
+++ app/main.py
@@ -4,14 +4,24 @@ from app.models import AnalysisRequest
 from app.services import get_claude_feedback
 import logging
 
+import os
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
+if os.getenv("ENVIRONMENT") == "production": # Verificar si el ambiente es producción o no
+    allowed_origin = ["https://sentisoft-2025-front.vercel.app/"] # Hay que poner el dominio real
+
+else:
+    allowed_origin = ["http://localhost:5173", "*"] 
+
+
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "*"],  
+    allow_origins=allowed_origin,  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
