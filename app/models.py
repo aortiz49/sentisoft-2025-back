3e72a3297d4e036c555f03858a53e832c85b801f
Andy Ortiz <andyortiz93@gmail.com>
2025-05-06 22:21:19 -0500

    defaults b2c true

--- app/models.py
+++ app/models.py
@@ -18,7 +18,7 @@ class User(BaseEntity):
     __tablename__ = "users"
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
-    b2c = Column(Boolean, nullable=False, default=False)
+    b2c = Column(Boolean, nullable=False, default=True)
     interview_count = Column(Integer, nullable=False, default=0)
     linkedin_url = Column(String, nullable=True)
     clarity_level = Column(Integer, ForeignKey("competency_levels.id"), nullable=True)
